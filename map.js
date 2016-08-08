031458a880d22275f54c376e500fb4ee4df1e4c1
Jesse "RyuOnato" Peters <Ryuonato@gmail.com>
2016-08-07 19:23:35 -0700

    Update map.js

--- map.js
+++ map.js
@@ -8,40 +8,30 @@ function initialize() {
             style: google.maps.NavigationControlStyle.SMALL,
           }
         };
-var map = new google.maps.Map(document.getElementById("map"),mapOptions);
-      
 
-    
+var map = new google.maps.Map(document.getElementById("map"),mapOptions);
+
 var layer = new google.maps.FusionTablesLayer({
-  query: {
-    select: 'location',
-    from: '1BkkG2nCVzFvvott2H2gPUb0eghfR7Nk37UZ76wQ',
-  		},
+	query: {
+		select: 'location',
+		from: '1BkkG2nCVzFvvott2H2gPUb0eghfR7Nk37UZ76wQ',
+  	}, 
   	options: {
   		styleId: 4,
   		templateId: 5
-  	},  
-    styles: [
-      { markerOptions: 
-        {iconName: 'red_blank'}
+  	},
+  	styles: [{
+  		markerOptions: {iconName: 'red_blank'}
       }]
-  
-  
-  
 });
+
 layer.setMap(map);
-
-
 }
-
-
-
 
 google.maps.event.addDomListener(window, 'load', initialize);
 
 $(document).ready(function(){
 	console.log('ready!');
-		
 	$("#check1").change(function() {
   		var $input = $(this);
   		if ($input.is(':checked')) {
